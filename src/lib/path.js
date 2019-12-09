e7fa23b1aa2d0bf24a293d33aa29db19af9f588c
Zhang GuangLin <zhangguanglin@ZhangdeMacBook-Pro.local>
2019-12-09 20:30:58 +0800

    add demo

--- src/lib/path.js
+++ src/lib/path.js
@@ -15,17 +15,17 @@ export default class Path extends Component {
     ctx.lineTo(50, 10);
     ctx.lineTo(30, 30);
     ctx.closePath();
-    ctx.stroke()
+    ctx.stroke();
   }
   showArc = () => {
     const e = document.getElementById('canvas');
     const ctx = e.getContext('2d');
     ctx.beginPath();
-    ctx.arc(100, 75, 50, Math.PI, Math.PI*2, true)
+    ctx.arc(100, 75, 50, Math.PI, Math.PI*2, true);
     ctx.stroke();
-    ctx.moveTo(200,75)
+    ctx.moveTo(200,75);
     ctx.arc(200, 75, 40, 0, Math.PI*2, true);
-    ctx.stroke()
+    ctx.stroke();
   }
   showQuadraticCurveTo = () => {
     const e = document.getElementById('canvas');
@@ -33,7 +33,7 @@ export default class Path extends Component {
     ctx.beginPath();
     ctx.moveTo(75, 50);
     ctx.quadraticCurveTo(25,100,50,120);
-    ctx.stroke()
+    ctx.stroke();
   }
   render(){
     return(
